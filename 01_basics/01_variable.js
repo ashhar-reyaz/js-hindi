d7c16453b548a8c084cb51a7ab118a977e8b6118
ashhar-reyaz <ashharreyaz82108@gmail.com>
2026-01-01 19:24:25 +0530

    revamp: cleaned up the devcontainer

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "ashhar@google.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+
+//accountId = 2 //not allowed
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Benguluru"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity])
